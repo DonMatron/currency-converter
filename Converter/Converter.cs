023c1aaf1190b2d5a043b9c3a153104963ae1052
Danylo Prudkyi <endermatrix.mmos@gmail.com>
2024-06-17 23:07:15 +0200

    Add project files.

---
+++ Converter/Converter.cs
@@ -0,0 +1,17 @@
+﻿namespace Converter
+{
+    public class Converter
+    {
+        public bool InputStringNumbersAreValid(string? Numbers)
+        {
+
+
+            throw new NotImplementedException();
+        }
+
+        public string ConvertNumbersToString(string? Numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CurrencyConverter/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter.Converter converter = new Converter.Converter();
+
+            bool repeat = true;
+
+            while (repeat)
+            {
+                RequestInput();
+
+                var input = Console.ReadLine();
+
+                while (!converter.InputStringNumbersAreValid(input))
+                {
+                    WriteInvalidInput();
+                    RequestInput();
+                    input = Console.ReadLine();
+                }
+
+                WriteOutput(converter.ConvertNumbersToString(input));
+                repeat = RequestRepeat();
+            }
+        }
+
+        private static void RequestInput()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Enter the sum:");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static bool RequestRepeat()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Again? (type y)");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            var response = Console.ReadLine();
+
+            if (response == "y")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static void WriteInvalidInput()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid input.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static void WriteOutput(string output)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(output);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
